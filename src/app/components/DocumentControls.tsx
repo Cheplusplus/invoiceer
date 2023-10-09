67c4e248c70ee50849bbb4b80f0fdf0515738b70
ChePlusPlus <chephoto@rocketmail.com>
2023-10-10 00:37:39 +0200

    Adds Buttons to the DocumentControl component

--- src/app/components/DocumentControls.tsx
+++ src/app/components/DocumentControls.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { Container, Typography, Box, Paper, Button } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DownloadIcon from "@mui/icons-material/Download";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+
+const sidebarIcons = [
+  [<DownloadIcon />],
+  [<EditIcon />],
+  [<ContentCopyIcon />],
+];
 
 const DocumentControls = () => {
-  return <div></div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        bgcolor: "#D4D4D4",
+        width: "48px",
+        position: "fixed",
+        top: "0",
+        right: "0",
+        height: "100vh",
+        pt: 4,
+      }}
+    >
+      {sidebarIcons.map((icon, i) => (
+        <Button
+          key={i}
+          variant="outlined"
+          sx={{
+            minWidth: "0",
+            maxWidth: "fit-content",
+            p: 0,
+            m: "0 auto",
+            mt: 4,
+            color: "#646464",
+            borderColor: "#646464",
+          }}
+        >
+          {icon}
+        </Button>
+      ))}
+    </Box>
+  );
 };
 
 export default DocumentControls;
--- src/app/components/PageContent.tsx
+++ src/app/components/PageContent.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { Container, Typography, Box, Paper } from "@mui/material";
 import SiteLogo from "./SiteLogo";
+import DocumentControls from "./DocumentControls";
 
 const PageContent = () => {
   return (
-    <Paper
-      sx={{
-        maxWidth: "921px",
-        maxHeight: "90vh",
-        aspectRatio: "210/280",
-        bgcolor: "white",
-        m: "auto",
-        position: "relative",
-        top: "50%",
-        transform: "translateY(-50%)",
-      }}
-      elevation={24}
-    ></Paper>
+    <>
+      <Paper
+        sx={{
+          maxWidth: "921px",
+          maxHeight: "90vh",
+          aspectRatio: "210/280",
+          bgcolor: "white",
+          m: "auto",
+          position: "relative",
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+        elevation={24}
+      ></Paper>
+      <DocumentControls></DocumentControls>
+    </>
   );
 };
 

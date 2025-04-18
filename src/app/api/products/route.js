266d3c95612aeed676fc31be551792c2725a542b
ChePlusPlus <chephoto@rocketmail.com>
2025-04-18 20:28:54 +0200

    Adds Puppeteer

--- src/app/api/products/route.js
+++
@@ -1,7 +0,0 @@
-export async function GET(req) {
-  const data = { message: "Hello" }
-  return new Response(JSON.stringify(data), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  })
-}
